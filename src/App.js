8b527f3657e02f153113d8a13d9e39870ac9d3c8
Aliyu Nurudeen <deen.aliyu40@gmail.com>
2023-05-20 10:53:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <img src={require("./assets/alleyb logo_Primary logo - White.png")} alt='' style={{ width: "200px" }} />
+          </div>
+          <div className="social">
+            <a href="https://facebook.com/alleybookings" title="Facebook" target="_blank" rel="noopener noreferrer">
+              <IconFacebook className="icon" />
+            </a>
+            <a href="https://twitter.com/alleybookings" title="Twitter" target="_blank" rel="noopener noreferrer">
+              <IconTwitter className="icon" />
+            </a>
+            <a href="https://github.com" title="GitHub" target="_blank" rel="noopener noreferrer">
+              <IconGithub className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Coming Soon.</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:support@alleybookings.com">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+          <span>2023 &copy; All right reseversed. Alleybookings.</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
